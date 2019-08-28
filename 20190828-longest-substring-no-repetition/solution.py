2da1b16af6585c3b17a65dbee6ebca066dda4e62
Patrick Reinbold <reinpa@outlook.de>
2019-08-28 17:13:15 +0200

    now it's the right one

--- 20190828-longest-substring-no-repetition/solution.py
+++ 20190828-longest-substring-no-repetition/solution.py
@@ -1,14 +1,23 @@
 class Solution:
   def lengthOfLongestSubstring(self, s):
-    solution = ""
+    length = 0
+    longest = 0
     prev = None
+    
     for char in s:
       curr = char
+      length = length + 1
       if curr is prev:
-        break
-      solution += curr
+        if length > longest: longest = length
+        length = 0
       prev = curr
-    return solution
+
+    if length > longest: longest = length # in case there is no repetition in the end
+
+    return longest
 
 print Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx')
 # 10
+print Solution().lengthOfLongestSubstring('dddhjkegkergaukgfbbbasdfmbalkgvlertv.awirghasddddddsfgr')
+# 27
+print Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxxasdfasdfasdfasdf')
