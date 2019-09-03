4e924264a0bccd536c368df9c8e0a3c3e5ac15ee
Patrick Reinbold <reinpa@outlook.de>
2019-09-03 16:50:25 +0200

    doneeee. seems pretty airtight mathematically.

--- 20190903-add-two-in-list/solution.py
+++ 20190903-add-two-in-list/solution.py
@@ -1,5 +1,16 @@
 def two_sum(list, k):
-  # Fill this in.
+  subtracted = set()
+  # we create a set containing all possible k - num
+  # since we are looking for a num in list that fulfills
+  # num + x = k we can store all possible k - num
+  # then, if another num2 = k - num we know that num + num2 = k
+  for num in list:
+    if num in subtracted: return True
+    subtracted.add(k - num)
+
+  return False
 
 print two_sum([4,7,1,-3,2], 5)
 # True
+
+print two_sum([5,1,2,3], 6)
