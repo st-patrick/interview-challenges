c402c6b9672de97dc082494eab8576d71c9ce498
Patrick Reinbold <reinpa@outlook.de>
2019-09-05 16:59:39 +0200

    DONEeeee. simply check amount of decreasing "deltas". If more than 1, no

--- 20190905-non-decreasing-array/solution.py
+++ 20190905-non-decreasing-array/solution.py
@@ -1,7 +1,14 @@
 def check(lst):
-  # Fill this in.
+  pointer = 0
+  decreasing_counter = 0
+  while pointer < len(lst)-1:
+	if lst[pointer] > lst[pointer+1]: decreasing_counter += 1
+	pointer += 1
+  return decreasing_counter < 2
 
 print check([13, 4, 7])
 # True
 print check([5,1,3,2,5])
+# False
+print check([3,4,5,8,9,1,2,3,4,2])
 # False
