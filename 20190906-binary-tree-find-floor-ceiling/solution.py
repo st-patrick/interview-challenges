9a33a57536322bf9f87af5f6868c77f82fec6681
Patrick Reinbold <reinpa@outlook.de>
2019-09-06 16:39:38 +0200

    START find the next higher and or lower number in the tree

---
+++ 20190906-binary-tree-find-floor-ceiling/solution.py
@@ -0,0 +1,21 @@
+class Node:
+  def __init__(self, value):
+    self.left = None
+    self.right = None
+    self.value = value
+
+def findCeilingFloor(root_node, k, floor=None, ceil=None):
+  # Fill this in.
+
+root = Node(8) 
+root.left = Node(4) 
+root.right = Node(12) 
+  
+root.left.left = Node(2) 
+root.left.right = Node(6) 
+  
+root.right.left = Node(10) 
+root.right.right = Node(14) 
+
+print findCeilingFloor(root, 5)
+# (4, 6)
