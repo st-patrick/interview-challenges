ada7b2e37dbc0dd128ea116094c0e0942502d67a
Patrick Reinbold <reinpa@outlook.de>
2019-09-11 16:51:42 +0200

    initial idea

--- 20190911-minimal-edit-distance/solution.py
+++ 20190911-minimal-edit-distance/solution.py
@@ -1,5 +1,10 @@
+# Here's the idea: for every letter in the shorter or equal string, we find
+# the same letter in the other string with minimal distance. If there is
+# none, it's obviously a replacement candidate
+# Finally, we prioritize shorter distances over long distances if any "cross"
+
 def distance(s1, s2):
-  # Fill this in.
+  
          
 print distance('biting', 'sitting')
 # 2
