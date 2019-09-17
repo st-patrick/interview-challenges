774c3d3b926f80b8a70a5b5b9ef5a59145d882f4
Patrick Reinbold <reinpa@outlook.de>
2019-09-17 18:31:04 +0200

    oops

--- 20190917-grid-traversion-possibilities/solution.py
+++ 20190917-grid-traversion-possibilities/solution.py
@@ -1,5 +1,22 @@
+# again with the self-similarity.
+# obviously this applies: num_ways(m,n) = num_ways(m-1,n) + num_ways(m,n-1)
+# also, with some logic we know that a single column or row of length x
+# has only 1 way to be traversed (all the way right or down)
+# combined we get:
+
 def num_ways(n, m):
-  # Fill this in.
+	if n == 1:
+		return 1
+	if m == 1:
+		return 1
+
+	return num_ways(n-1,m) + num_ways(n,m-1)
+		
+
 
 print num_ways(2, 2)
 # 2
+print num_ways(3,2)
+# 3
+print num_ways(3,3)
+# 6
