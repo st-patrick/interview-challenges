e01281653742dd65d248cd91e1384a567be16653
Patrick Reinbold <reinpa@outlook.de>
2019-09-18 10:36:40 +0200

    another solution idea

--- 20190917-grid-traversion-possibilities/solution.py
+++ 20190917-grid-traversion-possibilities/solution.py
@@ -1,3 +1,12 @@
+# TODO create explicit function calculating the possibilities
+# by using the binomial coefficient
+# you can either see this is true by filling out a grid of
+# solutions and how they add up or you can imagine
+# the grid being a series of m-1 choices to go right (= 1)
+# and n-1 choices to go down (= 0). Then, all we need is a 
+# formula for how to calculate the permutations of m-1 1s and
+# n-1 0s
+
 # again with the self-similarity.
 # obviously this applies: num_ways(m,n) = num_ways(m-1,n) + num_ways(m,n-1)
 # also, with some logic we know that a single column or row of length x
@@ -21,7 +30,9 @@ print num_ways(3,2)
 print num_ways(3,3)
 # 6
 print num_ways(4,3)
-#10
+# 10
 print num_ways(6,8)
 print num_ways(8,6)
-#792
+# 792
+print num_ways(4,4)
+# 20
